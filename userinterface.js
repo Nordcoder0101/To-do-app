62c6180c4e811cc9c0a58ffd3d84ae192f82b7aa
Your Name <nords3x4@gmail.com>
2018-01-25 16:25:26 -0800

    added cdns to html

---
+++ userinterface.js
@@ -0,0 +1,13 @@
+'use strict'
+
+console.log('hello world')
+
+// function User() {
+//   this.name = name,
+//   this.
+// };
+
+
+$('#new-task').click(function() {
+  console.log ('hello, it clicked')
+});
